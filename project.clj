8c567caefd76906ccde1ffd41dfe8a43b894b07b
bill-baumgartner <bill.baumgartner@gmail.com>
2016-08-01 16:06:19 -0600

    added maven group name (edu.ucdenver.ccp) to the project name

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject hanalyzer-kabob-layer "0.1.0-SNAPSHOT"
+(defproject edu.ucdenver.ccp/hanalyzer-kabob-layer "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
