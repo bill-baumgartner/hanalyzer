fa9e63e8d642e5a144615ace90c324540949d1a6
bill-baumgartner <bill.baumgartner@gmail.com>
2016-08-01 14:54:42 -0600

    added rule to make hanalyzer-like edges based on shared pathway membership

---
+++ resources/hanalyzer/rules/edges/shared-pathway/aael/shared_pathway_edges.clj
@@ -0,0 +1,45 @@
+`{:name "aael-shared-pathway-edges"
+  :description "This rule produces reified edges between two hanalyzer nodes
+  that have GGPs that appear in the same KEGG pathway."
+  :head (;; creates a reified edge of type iaohan/SharedPathwayEdge that links
+         ;; the two hanalyzer nodes that denote GGPs that participate in a shared pathway
+         (?/edge rdf/type iaohan/SharedPathwayEdge) 
+         (?/edge iaohan/linksNode ?/node1)
+         (?/edge iaohan/linksNode ?/node2)
+         (?/edge iaohan/denotes ?/pathway)
+         (?/edge rdfs/label ?/edgeLabel))
+
+  :reify ([?/edge {:ln (:sha-1 iaohan/SharedPathwayEdge ?/node1 ?/node2 ?/pathway)
+                   :ns "iaohan" :prefix "HANEDGE_PW_"}])
+
+  :body (;; this rule is yellow fever mosquito-specific so we restrict to
+         ;; NCBITaxon 7159
+         (?/taxon_r owl/onProperty obo/RO_0002162)
+         (?/taxon_r rdf/type owl/Restriction)
+         (?/taxon_r owl/someValuesFrom ?/taxon)
+         (?/taxon [rdfs/subClassOf *] obo/NCBITaxon_7159)
+         (?/pathway rdfs/subClassOf ?/taxon_r) ;; the pathway must belong to the specified taxon
+         (?/pathway [rdfs/subClassOf *] obo/INO_0000003) ;; INO_0000003 = pathway
+         (?/pathway rdfs/label ?/edgeLabel)
+
+         ;; the pathway must have two different participants that
+         ;; are each denoted by a Hanalyzer node
+         (?/pathway rdfs/subClassOf ?/participant_r)
+         (?/participant_r rdf/type owl/Restriction)
+         (?/participant_r owl/onProperty obo/RO_0000057) ;; RO_0000057 = has_participant
+         (?/participant_r owl/someValuesFrom ?/ggp_sc_1)
+         (?/pathway rdfs/subClassOf ?/participant_r_2)
+         (?/participant_r_2 rdf/type owl/Restriction)
+         (?/participant_r_2 owl/onProperty obo/RO_0000057) ;; RO_0000057 = has_participant
+         (?/participant_r_2 owl/someValuesFrom ?/ggp_sc_2)
+         (!= ?/ggp_sc_1 ?/ggp_sc_2)
+
+         (?/ggp_sc_1 rdfs/subClassOf ?/ggp_1)
+         (?/node1 iaohan/denotes ?/ggp_1)
+
+         (?/ggp_sc_2 rdfs/subClassOf ?/ggp_2)
+         (?/node2 iaohan/denotes ?/ggp_2))
+
+  :options {:magic-prefixes [;;["franzOption_clauseReorderer" "franz:identity"]
+                             ["franzOption_chunkProcessingAllowed" "franz:yes"]]}
+  }
