6f6e45078b4da8966586764cb67aee5c11e5559d
bill-baumgartner <bill.baumgartner@gmail.com>
2016-08-01 15:39:35 -0600

    changed artifact name to hanalyzer-kabob-layer

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject hanalyzer "0.1.0-SNAPSHOT"
+(defproject hanalyzer-kabob-layer "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
